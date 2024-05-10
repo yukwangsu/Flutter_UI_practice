944f6d0001caaa8518d27f7c69bc30466d575cea
yukwangsu <86365727+yukwangsu@users.noreply.github.com>
2024-05-10 23:33:19 +0900

    Icon, Transform(scale, translate)

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:toonflix/widgets/button.dart';
+import 'package:toonflix/widgets/currency_card.dart';
 
 void main() {
   runApp(const App());
@@ -13,165 +14,144 @@ class App extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
           backgroundColor: const Color(0xFF181818),
-          body: Padding(
-              //전체 화면의 왼쪽, 오른쪽에 Padding을 추가한다.
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              //Column에 모든걸 넣는다.
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                //children에 Column에 들어갈 요소들이 들어간다.
-                children: [
-                  //위에 빈 공간을 만든다.
-                  const SizedBox(
-                    height: 60,
-                  ),
-                  //이름과 문구를 담고있는 Row.
-                  Row(
-                    //이름과 문구를 오른쪽으로 붙인다.
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.end,
-                        children: [
-                          const Text(
-                            'Hey, pangsu',
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 28,
-                              fontWeight: FontWeight.w800,
+          body: SingleChildScrollView(
+            child: Padding(
+                //전체 화면의 왼쪽, 오른쪽에 Padding을 추가한다.
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                //Column에 모든걸 넣는다.
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  //children에 Column에 들어갈 요소들이 들어간다.
+                  children: [
+                    //위에 빈 공간을 만든다.
+                    const SizedBox(
+                      height: 60,
+                    ),
+                    //이름과 문구를 담고있는 Row.
+                    Row(
+                      //이름과 문구를 오른쪽으로 붙인다.
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          children: [
+                            const Text(
+                              'Hey, pangsu',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 28,
+                                fontWeight: FontWeight.w800,
+                              ),
                             ),
-                          ),
-                          Text(
-                            'Welcome back',
-                            style: TextStyle(
-                              color: Colors.white.withOpacity(0.8),
-                              fontSize: 18,
+                            Text(
+                              'Welcome back',
+                              style: TextStyle(
+                                color: Colors.white.withOpacity(0.8),
+                                fontSize: 18,
+                              ),
                             ),
+                          ],
+                        )
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 50,
+                    ),
+                    Text(
+                      'Total Balance',
+                      style: TextStyle(
+                        fontSize: 22,
+                        color: Colors.white.withOpacity(0.8),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 5,
+                    ),
+                    const Text(
+                      '\$5,194,482',
+                      style: TextStyle(
+                        fontSize: 48,
+                        fontWeight: FontWeight.w600,
+                        color: Colors.white,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    const Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Button(
+                          text: 'Transfer',
+                          bgcolor: Color(0xfff1b33b),
+                          textColor: Colors.black,
+                        ),
+                        Button(
+                          text: 'Request',
+                          bgcolor: Color(0xff1f2123),
+                          textColor: Colors.white,
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 40,
+                    ),
+                    Row(
+                      //Row이므로 crossAxisAlignment는 수직방향(아래의 경우 바닥에 붙는다.)
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      //Row이므로 mainAxisAlignment는 수평방향(아래의 경우 가운데 공간이 생긴다.)
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Text(
+                          'Wallets',
+                          style: TextStyle(
+                            fontSize: 36,
+                            fontWeight: FontWeight.w600,
+                            color: Colors.white,
                           ),
-                        ],
-                      )
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 60,
-                  ),
-                  Text(
-                    'Total Balance',
-                    style: TextStyle(
-                      fontSize: 22,
-                      color: Colors.white.withOpacity(0.8),
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 5,
-                  ),
-                  const Text(
-                    '\$5,194,482',
-                    style: TextStyle(
-                      fontSize: 48,
-                      fontWeight: FontWeight.w600,
-                      color: Colors.white,
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 30,
-                  ),
-                  const Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Button(
-                        text: 'Transfer',
-                        bgcolor: Color(0xfff1b33b),
-                        textColor: Colors.black,
-                      ),
-                      Button(
-                        text: 'Request',
-                        bgcolor: Color(0xff1f2123),
-                        textColor: Colors.white,
-                      ),
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 40,
-                  ),
-                  Row(
-                    //Row이므로 crossAxisAlignment는 수직방향(아래의 경우 바닥에 붙는다.)
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    //Row이므로 mainAxisAlignment는 수평방향(아래의 경우 가운데 공간이 생긴다.)
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      const Text(
-                        'Wallets',
-                        style: TextStyle(
-                          fontSize: 36,
-                          fontWeight: FontWeight.w600,
-                          color: Colors.white,
                         ),
-                      ),
-                      Text(
-                        'View All',
-                        style: TextStyle(
-                          fontSize: 18,
-                          color: Colors.white.withOpacity(0.8),
+                        Text(
+                          'View All',
+                          style: TextStyle(
+                            fontSize: 18,
+                            color: Colors.white.withOpacity(0.8),
+                          ),
                         ),
-                      ),
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  Container(
-                    decoration: BoxDecoration(
-                      color: const Color(0xff1f2123),
-                      borderRadius: BorderRadius.circular(25),
+                      ],
                     ),
-                    child: Padding(
-                      padding: const EdgeInsets.all(30),
-                      child: Row(
-                        children: [
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              const Text(
-                                'Euro',
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 32,
-                                  fontWeight: FontWeight.w600,
-                                ),
-                              ),
-                              const SizedBox(
-                                height: 10,
-                              ),
-                              Row(
-                                children: [
-                                  const Text(
-                                    '6 428',
-                                    style: TextStyle(
-                                      color: Colors.white,
-                                      fontSize: 20,
-                                    ),
-                                  ),
-                                  const SizedBox(
-                                    width: 5,
-                                  ),
-                                  Text(
-                                    'EUR',
-                                    style: TextStyle(
-                                      color: Colors.white.withOpacity(0.8),
-                                      fontSize: 20,
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ],
-                          )
-                        ],
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    const CurrencyCard(
+                      name: 'Euro',
+                      code: "EUR",
+                      amount: '6,428',
+                      icon: Icons.euro_rounded,
+                      isInverted: false,
+                    ),
+                    Transform.translate(
+                      offset: const Offset(0, -20),
+                      child: const CurrencyCard(
+                        name: 'Bitcoin',
+                        code: "BTC",
+                        amount: '9,785',
+                        icon: Icons.currency_bitcoin,
+                        isInverted: true,
                       ),
                     ),
-                  ),
-                ],
-              ))),
+                    Transform.translate(
+                      offset: const Offset(0, -40),
+                      child: const CurrencyCard(
+                        name: 'Dollar',
+                        code: "USD",
+                        amount: '428',
+                        icon: Icons.attach_money_outlined,
+                        isInverted: false,
+                      ),
+                    ),
+                  ],
+                )),
+          )),
     );
   }
 }
---
+++ lib/widgets/currency_card.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+
+class CurrencyCard extends StatelessWidget {
+  final String name, code, amount;
+  final IconData icon;
+  final bool isInverted;
+
+  final _blackColor = const Color(0xff1f2123);
+
+  const CurrencyCard({
+    super.key,
+    required this.name,
+    required this.code,
+    required this.amount,
+    required this.icon,
+    required this.isInverted,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      //overflow가 발생했을 때 대처
+      clipBehavior: Clip.hardEdge,
+      decoration: BoxDecoration(
+        color: isInverted ? Colors.white : _blackColor,
+        borderRadius: BorderRadius.circular(25),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(30),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  name,
+                  style: TextStyle(
+                    color: isInverted ? _blackColor : Colors.white,
+                    fontSize: 32,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  children: [
+                    Text(
+                      amount,
+                      style: TextStyle(
+                        color: isInverted ? _blackColor : Colors.white,
+                        fontSize: 20,
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 5,
+                    ),
+                    Text(
+                      code,
+                      style: TextStyle(
+                        color: isInverted
+                            ? _blackColor
+                            : Colors.white.withOpacity(0.8),
+                        fontSize: 20,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            //아이콘의 크기만 변경한다. 주변의 크기에는 영향을 미치지 않음.
+            Transform.scale(
+              scale: 2.2,
+              //sized box를 사용하지 않고 위치를 이동: Transform.translate, offset
+              child: Transform.translate(
+                offset: const Offset(-5, 12),
+                //아이콘 사용(화살표 클릭하면 아이콘 미리보기 가능)
+                child: Icon(
+                  icon,
+                  color: isInverted ? _blackColor : Colors.white,
+                  size: 88,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
